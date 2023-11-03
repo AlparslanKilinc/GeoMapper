93ba950e9f830b29c9f8fc19054f82576a201096
Umair Hoda <iqbalumairi@gmail.com>
2023-11-03 11:31:49 -0400

    Modified model for geojson and added routes and controller
    -Changed geojson-model.js to include a new schema for geojson and added name property
    -Added api-router.js to routes folder
    -Added geojson-router.js to routes folder
    -Added geojson-controller.js to controllers folder
    -Implemented controller methods needed for outline selection
        - retrieve a list of id name pairs for all outlines
        - retrieve a single outline by id

---
+++ server/controllers/geojson-controller.js
@@ -0,0 +1,17 @@
+const Geo = require('../models/geojson-model.js');
+
+const getGeojsonIdNamePairs = async (req, res) => {
+  // retreive all geojson documents from the database but select only the _id and name fields
+  const geojson = await Geo.find({}, '_id name');
+  // send the geojson documents back to the client
+  res.json(geojson);
+};
+
+const getGeojsonById = async (req, res) => {
+  // retreive the geojson document with the specified id from the database
+  const geojson = await Geo.findById(req.params.id);
+  // send the geojson document back to the client
+  res.json(geojson);
+};
+
+module.exports = { getGeojsonById, getGeojsonIdNamePairs };
