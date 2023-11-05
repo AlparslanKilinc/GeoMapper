a58029b3323e23dc98e3871f343e96125d7e1327
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-11-05 10:13:47 -0500

    Implement navigation for map creation stages
    - Added a new component, MapCreationWrapper, which encapsulates
    the stage-based logic for map creation.
    
    - This component includes navigation buttons to move back and forth
    between different stages of the map creation
    
    - The navigation buttons are styled with a turquoise theme and are
    disabled appropriately at the start and end of the stages array.
    
    -The current stage is managed using a state hook and
    can be navigated with back and next buttons.

--- client/src/components/MapCreationWrapper.jsx
+++ client/src/components/MapCreationWrapper.jsx
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import '../styles/mapCreationWrapper.css';
+import { styled } from '@mui/material/styles';
+import TempleSelection from './MapCreation/TemplateSelection';
+import MapDataEditing from './MapCreation/MapDataEditing';
+import MapUpload from './MapCreation/MapUpload';
+import MapGraphicsEditing from './MapCreation/MapGraphicsEditing';
 
 export default function MapCreationWrapper() {
-  return <div>MapCreationWrapper</div>;
+  const stages = [<TempleSelection />, <MapUpload />, <MapDataEditing />, <MapGraphicsEditing />];
+  const [currentStage, setCurrentStage] = useState(0);
+
+  const NavigationButton = styled(Button)(({ theme }) => ({
+    borderColor: '#40e0d0',
+    color: '#40e0d0',
+    '&:hover': {
+      borderColor: '#40e0d0',
+      backgroundColor: 'transparent'
+    }
+  }));
+
+  const goBack = () => {
+    setCurrentStage(currentStage - 1);
+  };
+
+  const goForward = () => {
+    setCurrentStage(currentStage + 1);
+  };
+
+  return (
+    <div className="mapCreationWrapper">
+      <div className="wrapper-button-group">
+        <NavigationButton
+          variant="outlined"
+          startIcon={<ArrowBackIcon />}
+          onClick={goBack}
+          disabled={currentStage === 0}
+        >
+          Back
+        </NavigationButton>
+
+        <NavigationButton
+          variant="outlined"
+          endIcon={<ArrowForwardIcon />}
+          onClick={goForward}
+          disabled={currentStage === stages.length - 1}
+        >
+          Next
+        </NavigationButton>
+      </div>
+      <div>{stages[currentStage]}</div>
+    </div>
+  );
 }
