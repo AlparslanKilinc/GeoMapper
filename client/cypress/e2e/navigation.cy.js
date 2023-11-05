5a3cb8012416c7a1d5649ecb35b748d90e848015
Tina Mistry <tmistry10@gmail.com>
2023-11-04 21:24:20 -0400

    added navigation bar tests. to run on cypress: cd client npm run dev npm test

--- client/cypress/e2e/navigation.cy.js
+++ client/cypress/e2e/navigation.cy.js
@@ -1,7 +1,17 @@
 describe('Navigation Bar', () => {
-    it('redirects to the explore page when clicking the explore icon', () => {
+    it('renders the navigation bar', () => {
         cy.visit('/');
-
-        // To be implemented
+    });
+    it('renders the explore page on button click', () => {
+        cy.visit('/'); //visit home page
+        cy.get('#exploreButton')
+            .click() //click explore button
+            .url().should('include', '/explore'); //make sure new page is explore
+    });
+    it('renders the map creation page on button click', () => {
+        cy.visit('/'); //visit home page
+        cy.get('#mapCreation')
+            .click() //click map creation button
+            .url().should('include', '/mapCreation'); //make sure new page is map creation
     });
 });
