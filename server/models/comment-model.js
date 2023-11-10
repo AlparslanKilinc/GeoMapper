2242621f2a72128b8a327750a7efb126334da083
Tina Mistry <tmistry10@gmail.com>
2023-11-10 13:05:50 -0500

    added comments model

--- server/models/comment-model.js
+++ server/models/comment-model.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const {schema: userSchema} = require("./user-model");
 
-const comments = new Schema({
+const commentSchema = new mongoose.Schema({
     author: userSchema,
     text: { type: String, required: true },
     date_posted: { type: Date, required: true },
-    replies: [{ type:[comments], required: false}],
+    replies: [{ type:[commentSchema], required: false}],
     is_reply: { type: Boolean, required: true, default: false},
 });
+module.exports = mongoose.model("comment", commentSchema);
