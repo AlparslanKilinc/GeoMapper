a2ccf8e8e1bba06017f39b502480a65fdd238022
Tina Mistry <tmistry10@gmail.com>
2023-11-12 16:28:57 -0500

    added sort and filter buttons

--- client/src/components/ExplorePage/ExplorePage.jsx
+++ client/src/components/ExplorePage/ExplorePage.jsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../styles/explorePage.css'
 import MapCard from './MapCard'
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, CardActions } from '@mui/material';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from '@mui/material/Autocomplete';
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import Popover from '@mui/material/Popover';
+
 
 
 export default function ExplorePage() {
-  return (
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [filterAnchor, setFilterAnchor] = useState(null)
+    const isSortOpen = Boolean(anchorEl);
+    const isFilterOpen = Boolean(filterAnchor);
+    const tags = [
+        {name: 'u.s.a', id: 1},
+        {name: 'choropleth', id: 2},
+        {name: 'america', id: 3},
+        {name: 'election', id : 4}
+    ];
+
+    const handleSortMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleSortMenuClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleFilterMenuOpen = (event) => {
+        setFilterAnchor(event.currentTarget);
+    }
+
+    const handleFilterMenuClose = () => {
+        setFilterAnchor(null)
+    }
+    return (
       <div className = "explorePage">
         <div className = "heading"><h1>Explore</h1></div>
         <div className = "buttons">
           <div className = "filter"><Button
               variant = "contained"
+              onClick={handleFilterMenuOpen}
               sx = {{backgroundColor : 'var(--main-color)',
                     '&:hover': {backgroundColor: 'var(--dark-color)'}}}
           >Filter</Button>
@@ -23,9 +52,11 @@ export default function ExplorePage() {
           <div className = "sort">
               <Button
                   variant = "contained"
+                  onClick={handleSortMenuOpen}
                   sx = {{backgroundColor : 'var(--main-color)',
-                      '&:hover': {backgroundColor: 'var(--dark-color)'}}}
-              >Sort</Button>
+                          '&:hover': {backgroundColor: 'var(--dark-color)'
+                  }}}>Sort
+              </Button>
           </div>
         </div>
         <div className = "content">
@@ -45,6 +76,67 @@ export default function ExplorePage() {
                 <MapCard></MapCard>
             </div>
         </div>
+
+
+          <Menu anchorEl={anchorEl}
+                open={isSortOpen}
+                onClose={handleSortMenuClose}>
+              <MenuItem onClick={handleSortMenuClose}>Newest</MenuItem>
+             <MenuItem onClick={handleSortMenuClose}>Oldest</MenuItem>
+             <MenuItem onClick={handleSortMenuClose}>Most Likes</MenuItem>
+              <MenuItem onClick={handleSortMenuClose}>Least Likes</MenuItem>
+          </Menu>
+
+          <Popover
+              open={isFilterOpen}
+              anchorEl={filterAnchor}
+              onClose={handleFilterMenuClose}
+              anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left', // Adjust this to align with the left of the button
+              }}
+              transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left', // Adjust this to align with the left of the button
+              }}
+          >
+              <div style={{ padding: '10px' }}>
+                  <Autocomplete
+                      multiple
+                      id="tags"
+                      options={tags}
+                      getOptionLabel={(tag) => tag.name}
+                      default={[tags[2]]}
+                      filterSelectedOptions
+                      renderInput={(params) => (
+                          <TextField
+                              {...params}
+                              label="Search Tags"
+                              placeholder="Select Tags"
+
+                          />
+                      )}
+                  />
+                  <div style={{ marginTop: '10px' }}>
+
+                      {tags.map((tag) => (
+                          <Chip
+                              key={tag.id}
+                              label={tag.name}
+                              clickable
+                              onClick={() => {
+                                  console.log(`Clicked on tag: ${tag.name}`);
+                              }}
+                              style={{ margin: '4px' }}
+                          />
+                      ))}
+                  </div>
+              </div>
+          </Popover>
+
+
+
+
 
       </div>
   );
