8e968387578e52f158a88368bd38abe412e358fc
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-11-13 14:02:26 -0500

    Add dynamic navigation in ProfileNavBar component
    Implemented a dynamic navigation bar in the ProfileNavBar component to
    switch between Drafts, Bookmarks, and PublishedMaps.
    The navbar uses state management to control which tab is active
    and to display the corresponding content.
    Added appropriate styling for active tabs and integrated
    Material UI icons for visual enhancement.

---
+++ client/src/components/UserProfile/ProfileNavBar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import '../../styles/profileNavBar.css';
+import Drafts from './Drafts';
+import Bookmarks from './Bookmarks';
+import PublishedMaps from './PublishedMaps';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import EditIcon from '@mui/icons-material/Edit';
+import MapIcon from '@mui/icons-material/Map';
+
+const NavState = {
+  DRAFTS: 'Drafts',
+  BOOKMARKS: 'Bookmarks',
+  PUBLISHED_MAPS: 'PublishedMaps'
+};
+
+export default function ProfileNavBar() {
+  const [Select, setSelect] = useState(NavState.DRAFTS);
+  const [Content, setContent] = useState(<Drafts />);
+
+  let handleButton = (input) => {
+    switch (input) {
+      case NavState.BOOKMARKS:
+        setSelect(NavState.BOOKMARKS);
+        setContent(<Bookmarks />);
+        break;
+      case NavState.DRAFTS:
+        setSelect(NavState.DRAFTS);
+        setContent(<Drafts />);
+        break;
+      case NavState.PUBLISHED_MAPS:
+        setSelect(NavState.PUBLISHED_MAPS);
+        setContent(<PublishedMaps />);
+        break;
+      default:
+        setSelect(NavState.DRAFTS);
+        setContent(<Drafts />);
+        break;
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <li className="nav-list">
+        <button
+          onClick={() => handleButton(NavState.DRAFTS)}
+          className="nav-button"
+          style={{ borderBottom: Select === NavState.DRAFTS ? '3px solid #40e0d0' : 'none' }}
+        >
+          <EditIcon />
+          Drafts
+        </button>
+
+        <button
+          onClick={() => handleButton(NavState.PUBLISHED_MAPS)}
+          className="nav-button"
+          style={{
+            borderBottom: Select === NavState.PUBLISHED_MAPS ? '3px solid #40e0d0' : 'none'
+          }}
+        >
+          <MapIcon />
+          Published Maps
+        </button>
+
+        <button
+          onClick={() => handleButton(NavState.BOOKMARKS)}
+          className="nav-button"
+          style={{ borderBottom: Select === NavState.BOOKMARKS ? '3px solid #40e0d0' : 'none' }}
+        >
+          <BookmarkBorderIcon />
+          Bookmarks
+        </button>
+      </li>
+      <div className="content">{Content}</div>
+    </div>
+  );
+}
