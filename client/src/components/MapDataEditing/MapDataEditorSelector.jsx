16684d4137573b32190c250b7b358eddb486ae0d
Jun Yan <jun.yan.1@stonybrook.edu>
2023-11-12 20:44:37 -0500

    Finished all the rest of the map editor, included Heat map, Dot density map and Spike map.

--- client/src/components/MapDataEditing/MapDataEditorSelector.jsx
+++ client/src/components/MapDataEditing/MapDataEditorSelector.jsx
@@ -1,6 +1,9 @@
 import { useSelector } from 'react-redux';
 import SymbolmapDataEditor from './SymbolmapDataEditor';
 import ChoroplethmapDataEditor from './ChoroplethmapDataEditor';
+import HeatmapDataEditor from './HeatmapDataEditor';
+import DotdensitymapDataEditor from './DotdensitymapDataEditor';
+import SpikemapDataEditor from './SpikemapDataEditor';
 
 const MapDataEditorSelector = () => {
     const mapGraphicsType = useSelector((state) => state.mapMetadata.mapGraphicsType);
@@ -10,6 +13,12 @@ const MapDataEditorSelector = () => {
             return <SymbolmapDataEditor />;
         case "Choropleth Map":
             return <ChoroplethmapDataEditor />;
+        case "Heat Map":
+            return <HeatmapDataEditor />;
+        case "Dot Density Map":
+            return <DotdensitymapDataEditor />;
+        case "Spike Map":
+            return <SpikemapDataEditor />;
         default:
             return null; // or some default component or behavior
     }
