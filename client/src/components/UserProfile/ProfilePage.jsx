a330b89ddab56c6251d08714063be0a0b2f69255
Alparslan Kilinc <100721307+AlparslanKilinc@users.noreply.github.com>
2023-11-13 15:43:32 -0500

    Add ForgotPassword Stages

--- client/src/components/UserProfile/ProfilePage.jsx
+++ client/src/components/UserProfile/ProfilePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Sidebar from './SideBar';
+import CopyRight from '../CopyRight';
 import '../../styles/profilePage.css';
 import ProfileNavBar from './ProfileNavBar';
 
 export default function ProfilePage() {
   return (
-    <div className="profile">
-      <Sidebar />
-      <ProfileNavBar />
+    <div className='profile-page'>
+      <div className="profile">
+        <Sidebar />
+        <ProfileNavBar />
+      </div>
+      <CopyRight />
     </div>
   );
 }
