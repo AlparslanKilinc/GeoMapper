465bd943d8b77c6f765ee43926a10005a7db3f6e
Tina Mistry <tmistry10@gmail.com>
2023-11-18 21:32:36 -0500

    upload dark mode toggle

---
+++ client/src/components/ToggleDarkMode.jsx
@@ -0,0 +1,26 @@
+import  React from 'react';
+import '../styles/toggleDarkMode.css'
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import {Switch} from "@mui/material";
+
+
+export default function ToggleDarkMode ({handleDarkModeClick, isDark}) {
+
+    return(
+
+    <div className = "toggle-container">
+        <IconButton onClick={handleDarkModeClick} color="inherit">
+            {isDark ? <Brightness4Icon /> : <Brightness7Icon />}
+        </IconButton>
+
+
+
+    </div>
+
+
+
+    );
+
+}
