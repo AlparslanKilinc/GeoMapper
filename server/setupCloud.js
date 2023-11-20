193e83463099c0489304e81653d5c16fe4d4c5c4
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-11-20 18:42:17 -0500

    testing cloud key json

--- server/setupCloud.js
+++ server/setupCloud.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 if (process.env.GOOGLE_CLOUD_KEY) {
-  const keyFilePath = path.join(__dirname, 'google-cloud-key.json');
-  fs.writeFileSync(keyFilePath, process.env.GOOGLE_CLOUD_KEY);
-
+  const keyFilePath = path.join('/tmp', 'google-cloud-key.json');
+  const keyData = JSON.parse(process.env.GOOGLE_CLOUD_KEY);
+  fs.writeFileSync(keyFilePath, JSON.stringify(keyData));
   process.env.GOOGLE_APPLICATION_CREDENTIALS = keyFilePath;
 }
