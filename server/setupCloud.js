8e519091ae0cbd9923a9273d449207a5fc117929
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-11-20 19:26:54 -0500

    setting google cloud credentials

--- server/setupCloud.js
+++ server/setupCloud.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-if (process.env.GOOGLE_CLOUD_KEY) {
+const secretFilePath = '/etc/secrets/google-cloud-key.json';
+
+if (fs.existsSync(secretFilePath)) {
+  const keyData = JSON.parse(fs.readFileSync(secretFilePath, 'utf8'));
+
   const keyFilePath = path.join('/tmp', 'google-cloud-key.json');
-  const keyData = JSON.parse(process.env.GOOGLE_CLOUD_KEY);
   fs.writeFileSync(keyFilePath, JSON.stringify(keyData));
+
   process.env.GOOGLE_APPLICATION_CREDENTIALS = keyFilePath;
 }
