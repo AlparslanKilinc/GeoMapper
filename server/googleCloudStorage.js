0ed20055517fee60bd6c824bce26270a743d6617
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-11-20 19:49:52 -0500

    removing key file name since it is already set

--- server/googleCloudStorage.js
+++ server/googleCloudStorage.js
@@ -1,9 +1,6 @@
 const { Storage } = require('@google-cloud/storage');
 
-const storage = new Storage({
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
-});
-
+const storage = new Storage();
 const bucket = storage.bucket(process.env.GCS_BUCKET);
 
 module.exports = { storage, bucket };
