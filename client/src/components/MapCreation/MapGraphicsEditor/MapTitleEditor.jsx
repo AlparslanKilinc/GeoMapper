ccc15207ace54586dea6b42bdfb4758a428c1ba0
Jun Yan <jun.yan.1@stonybrook.edu>
2023-11-24 23:12:53 -0500

    Adjust TextField width to dynamically resize based on input content

--- client/src/components/MapCreation/MapGraphicsEditor/MapTitleEditor.jsx
+++ client/src/components/MapCreation/MapGraphicsEditor/MapTitleEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import { useSelector, useDispatch } from 'react-redux';
@@ -7,33 +7,55 @@ import { changeMapTitle } from '../../../redux-slices/mapMetadataSlice';
 const MapTitleEditor = () => {
   const dispatch = useDispatch();
   const { title } = useSelector((state) => state.mapMetadata);
+  const [width, setWidth] = useState(0);
+  const textWidthRef = useRef();
+
   const handleTitleChange = (event) => {
     dispatch(changeMapTitle(event.target.value));
   };
 
+  useEffect(() => {
+    setWidth(textWidthRef.current.scrollWidth);
+  }, [title]);
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" p={1}>
-      <TextField
-        value={title}
-        onChange={handleTitleChange}
-        variant="outlined"
-        placeholder="Enter document title"
-        sx={{
-          '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-              borderColor: 'transparent' // Hide border by default
+      <div style={{ position: 'relative' }}>
+        <span ref={textWidthRef} style={{
+          position: 'absolute',
+          visibility: 'hidden',
+          height: 'auto',
+          width: 'auto',
+          whiteSpace: 'nowrap',
+          fontWeight: 'bold',
+          fontSize: '1.25rem',
+          padding: '0.7em 0.4em'
+        }}>
+          {title || 'Enter document title'}
+        </span>
+        <TextField
+          value={title}
+          onChange={handleTitleChange}
+          variant="outlined"
+          placeholder="Enter document title"
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'transparent' // Hide border by default
+              },
+              '&:hover fieldset': {
+                borderColor: 'grey' // Show border on hover
+              },
+              '& input': {
+                fontSize: '1.25rem',
+                fontWeight: 'bold',
+                padding: '0.7em 0.4em'
+              },
             },
-            '&:hover fieldset': {
-              borderColor: 'grey' // Show border on hover
-            },
-            '& input': {
-              fontSize: '1.25rem',
-              fontWeight: 'bold',
-              padding: '0.7em 0.4em'
-            },
-          }
-        }}
-      />
+            width: `${width}px`
+          }}
+        />
+      </div>
     </Box>
   );
 };
