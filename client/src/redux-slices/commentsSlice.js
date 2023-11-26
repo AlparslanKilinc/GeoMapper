5d07cae99f4d0c30c898b6b6e16fd078b644eed4
Tina Mistry <tmistry10@gmail.com>
2023-11-26 13:39:31 -0500

    added comments slice

--- client/src/redux-slices/commentsSlice.js
+++ client/src/redux-slices/commentsSlice.js
@@ -1,45 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-// Map Metadata Slice
-const commentsInitialState = {
-    comments: []
+const initialState = {
+    mapId: null, // Reference to the main Map
+    author: '',
+    text: '',
+    date_posted: new Date(),
+
 };
 
-const commentsDataSlice = createSlice({
-    name: "commentsData",
-    initialState: commentsInitialState,
+const commentsSlice = createSlice({
+    name: 'mapStyles',
+    initialState,
     reducers: {
-        addComment: (state, action) => {
-                const newComment = {
-                    author: action.payload.author,
-                    datePosted: new Date(),
-                    text: action.payload.text,
-                    replies: [],
-                    is_reply:false
-                };
-                state.comments.push(newComment);
+        changeAuthor: (state, action) => {
+            state.shape = action.payload;
         },
-        replyToComment: (state, action) => {
-           const {parentId, replyText, author} = action.payload;
-           const commentToReply = state.comments.find(comment => comment.id === parentId);
-            if (commentToReply) {
-                const newReply = {
-                    id: Date.now(), // You might want to use a more robust ID generation method
-                    author: author,
-                    datePosted: new Date(),
-                    text: replyText,
-                    is_reply: true,
-                };
-
-                commentToReply.replies.push(newReply);
-            }
-
-        },
-        removeComment: (state, action) => {
-            const commentIdToRemove = action.payload;
-            state.comments = state.comments.filter(comment => comment.id !== commentIdToRemove);
+        changeText: (state, action) => {
+            state.shape = action.payload;
         },
     }
 });
-export const { addComment, replyToComment, removeComment } = commentsDataSlice.actions;
-export default commentsDataSlice.reducer;
+
+export const {
+    changeAuthor,
+    changeText
+
+
+} = commentsSlice.actions;
+
+export default commentsSlice.reducer;
