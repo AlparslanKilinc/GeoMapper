343eac25b86191b75d1b10594b26526ebe2c9f09
Tina Mistry <tmistry10@gmail.com>
2023-11-26 21:19:10 -0500

    added transparent background to all map graphic images

--- client/src/components/MapCreation/TemplateSelection.jsx
+++ client/src/components/MapCreation/TemplateSelection.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import '../../styles/templateSelection.css';
-import heatMap from '../../assets/heat_map.jpeg';
+import heatMap from '../../assets/heat_map.png';
 import spikeMap from '../../assets/spike_map.png';
 import symbolMap from '../../assets/symbol_map.png';
 import choroplethMap from '../../assets/choropleth_map.png';
