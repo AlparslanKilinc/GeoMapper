937b8a5478510bc8dd947dad6dbccd73297a6844
Jun Yan <jun.yan.1@stonybrook.edu>
2023-11-26 23:25:40 -0500

    Fix label alignment issue

--- client/src/components/MapCreation/Map/GeojsonWrapper.jsx
+++ client/src/components/MapCreation/Map/GeojsonWrapper.jsx
@@ -32,7 +32,7 @@ export default function GeojsonWrapper({ isStyled }) {
 
   const generateRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
 
-  let onEachFeature = (feature, layer) => {};
+  let onEachFeature = (feature, layer) => { };
 
   useEffect(() => {
     if (geojsonLayer) {
