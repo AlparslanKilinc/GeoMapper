6ee386d831b97db43c132adb582c1ac2a1112bc2
Umair Hoda <iqbalumairi@gmail.com>
2023-12-08 09:02:43 -0500

    Optimize/dot density
    * Optimized dot density layer by using CanvaaRenderingContext2D instead of Markers. This is a 10x speedup.
    ```
    
    * Fixed bug where shape menu wouldn't render for dot-density map
    
    * Added shapes for dot density

--- client/src/components/MapCreation/MapGraphicsEditor/StylesMenu/StylesMenus.jsx
+++ client/src/components/MapCreation/MapGraphicsEditor/StylesMenu/StylesMenus.jsx
@@ -4,23 +4,25 @@ import ShapeAccordionMenu from './Shapes/ShapeAccordionMenu';
 import BorderAccordionMenu from './BorderAccordionMenu';
 import MapStyleAccordionMenu from './MapStyleAccordionMenu';
 import LabelsAccordionMenu from './LabelsAccordionMenu';
-import LegendAccordionMenu from "../Legend/LegendAccordionMenu.jsx";
+import LegendAccordionMenu from '../Legend/LegendAccordionMenu.jsx';
 import { useSelector } from 'react-redux';
 
 export default function StylesMenus() {
   const SYMBOL_MAP = 'Symbol Map';
   const SPIKE_MAP = 'Spike Map';
-  const DOT_DENSITY = 'Dot Density';
+  const DOT_DENSITY = 'Dot Density Map';
   const mapGraphicsType = useSelector((state) => state.mapMetadata.mapGraphicsType);
 
   return (
     <div>
       <ColorsAccordionMenu />
-      {(mapGraphicsType === SYMBOL_MAP || mapGraphicsType === SPIKE_MAP || mapGraphicsType === DOT_DENSITY) && <ShapeAccordionMenu />}
+      {(mapGraphicsType === SYMBOL_MAP ||
+        mapGraphicsType === SPIKE_MAP ||
+        mapGraphicsType === DOT_DENSITY) && <ShapeAccordionMenu />}
       <BorderAccordionMenu />
       <LabelsAccordionMenu />
       <MapStyleAccordionMenu />
-      <LegendAccordionMenu/>
+      <LegendAccordionMenu />
     </div>
   );
 }
