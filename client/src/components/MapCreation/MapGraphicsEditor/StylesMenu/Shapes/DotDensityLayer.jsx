95fad83d615087eaee4e9f1708e25e20b08c9097
Umair Hoda <iqbalumairi@gmail.com>
2023-12-07 19:59:47 -0500

    Optimized dot density layer by using CanvaaRenderingContext2D instead of SVG. This is a 10x speedup. ```

--- client/src/components/MapCreation/MapGraphicsEditor/StylesMenu/Shapes/DotDensityLayer.jsx
+++ client/src/components/MapCreation/MapGraphicsEditor/StylesMenu/Shapes/DotDensityLayer.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Marker } from 'react-leaflet';
-import shapeIconMap from './shapeIconMap';
+import React, { useEffect, useRef } from 'react';
+import { useMap } from 'react-leaflet';
 import { useSelector } from 'react-redux';
 import * as Turf from '@turf/turf';
 
-const DotDensityLayer = () => {
+const DotDensityCanvasLayer = () => {
+  const map = useMap();
+  const canvasRef = useRef(null);
+
   const shape = useSelector((state) => state.mapStyles.shape);
   const fixedSymbolSize = useSelector((state) => state.mapGraphics.fixedSymbolSize);
   const fixedOpacity = useSelector((state) => state.mapGraphics.fixedOpacity);
@@ -13,10 +15,6 @@ const DotDensityLayer = () => {
   const dotDensityByProperty = useSelector((state) => state.mapGraphics.dotDensityByProperty);
   const regions = useSelector((state) => state.mapGraphics.regions);
   const colors = useSelector((state) => state.mapStyles.colors);
-
-  const iconSize = fixedSymbolSize;
-  const opacity = fixedOpacity;
-
   const features = useSelector((state) => state.geojson.geojson.geoJSON.features);
 
   const generateRandomPointsInFeature = (feature, numberOfPoints) => {
@@ -30,28 +28,59 @@ const DotDensityLayer = () => {
     return points;
   };
 
-  const markers = [];
-  features.forEach((feature) => {
-    // calculate how many dots will go inside this feature
-    const regionDetails = regions[feature.properties.regionIdx];
-    dotDensityByProperty.forEach((property) => {
-      const dotDensityValue = regionDetails[property];
-      if (dotDensityValue === undefined) return;
-      let color = fixedColor;
-      const colorObj = colors.find((c) => c.name === property);
-      if (colorObj) color = colorObj.color;
-      const icon = shapeIconMap['circle'](iconSize, color, opacity) || shapeIconMap.default;
+  useEffect(() => {
+    if (!map || !canvasRef.current) return;
+    const canvas = canvasRef.current;
+    canvas.width = map.getSize().x;
+    canvas.height = map.getSize().y;
+    const ctx = canvas.getContext('2d');
 
-      const numberOfDots = Math.floor(dotDensityValue / valuePerDot);
-      const randomPoints = generateRandomPointsInFeature(feature, numberOfDots);
-      randomPoints.forEach((point) => {
-        const latLng = [point.geometry.coordinates[1], point.geometry.coordinates[0]];
-        markers.push(<Marker key={latLng.join('#')} position={latLng} icon={icon}  style={{opacity: opacity}} />);
+    const drawPoints = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      features.forEach((feature) => {
+        const regionDetails = regions[feature.properties.regionIdx];
+
+        dotDensityByProperty.forEach((property) => {
+          const dotDensityValue = regionDetails[property];
+          if (dotDensityValue === undefined) return;
+
+          let color = fixedColor;
+          const colorObj = colors.find((c) => c.name === property);
+          if (colorObj) color = colorObj.color;
+
+          const numberOfDots = Math.floor(dotDensityValue / valuePerDot);
+          const randomPoints = generateRandomPointsInFeature(feature, numberOfDots);
+
+          randomPoints.forEach((point) => {
+            const latLng = [point.geometry.coordinates[1], point.geometry.coordinates[0]];
+            const canvasPoint = map.latLngToContainerPoint(latLng);
+
+            // Draw the dot
+            ctx.fillStyle = color;
+            ctx.globalAlpha = fixedOpacity;
+            ctx.beginPath();
+            ctx.arc(canvasPoint.x, canvasPoint.y, fixedSymbolSize / 2, 0, Math.PI * 2);
+            ctx.fill();
+          });
+        });
       });
-    });
-  });
+    };
 
-  return <div>{markers}</div>;
+    drawPoints();
+    map.on('moveend', drawPoints);
+
+    return () => {
+      map.off('moveend', drawPoints);
+    };
+  }, [map, features, regions, colors, fixedSymbolSize, fixedOpacity, fixedColor, valuePerDot]);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ position: 'absolute', top: 0, left: 0, zIndex: 400, pointerEvents: 'none' }}
+    />
+  );
 };
 
-export default DotDensityLayer;
+export default DotDensityCanvasLayer;
