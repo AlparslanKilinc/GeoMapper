057db810eef86d6cfe7004dc0f8e46aa3d82fa9c
Umair Hoda <iqbalumairi@gmail.com>
2023-12-18 07:47:58 -0500

    UI/map view
    * Updated MapView
    * Changed the comment component

--- client/src/components/MapView/Comment.jsx
+++ client/src/components/MapView/Comment.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import '../../styles/commentSection.css'
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
-
-export default function Comment({key, comment}){
-    return(
-        <div className = 'comment-container'>
-            <img  src = {comment.authorProfilePicture}/>
-            <div>
-                <span className = "username">{comment.authorUsername}</span>
-                <span className="comment-text" style={{ whiteSpace: 'pre-line' }}>
-                {comment.text}
-                </span>
-            </div>
-        </div>
-    );
+export default function Comment({ comment }) {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'start', marginBottom: 2 }}>
+      <Avatar src={comment.authorProfilePicture} sx={{ marginRight: 2 }} />
+      <Box sx={{ maxWidth: '100%' }}>
+        {' '}
+        {/* Set a maximum width */}
+        <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>
+          {comment.authorUsername}
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            whiteSpace: 'pre-line',
+            wordBreak: 'break-word' // Enable text wrapping
+          }}
+        >
+          {comment.text}
+        </Typography>
+      </Box>
+    </Box>
+  );
 }
